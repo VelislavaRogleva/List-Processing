2e8656d4314be64ddfa0d3045070dbeebd9a2c36
VelislavaRogleva <v.rogleva@gmail.com>
2017-11-14 23:11:02 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<String> list = Arrays.asList(reader.readLine().split("\\s+"));
+        printList(list);
+
+        while (true) {
+            String[] commandArgs = reader.readLine().split("\\s+");
+            if (commandArgs[0].equals("end")) {
+                break;
+            }
+
+            switch (commandArgs[0]) {
+
+            }
+
+        }
+
+
+    }
+
+    private static void printList(List<String> list) {
+        System.out.println(String.join(" ", list));
+    }
+}
