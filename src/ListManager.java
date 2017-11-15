d158c88d189ef05b70c948f7ad1d44a1f5a43b26
VelislavaRogleva <v.rogleva@gmail.com>
2017-11-15 19:41:14 +0200

    Added append and prepend commands

--- src/ListManager.java
+++ src/ListManager.java
@@ -5,4 +5,22 @@ class ListManager {
     static void printList(List<String> list) {
         System.out.println(String.join(" ", list));
     }
+
+    static void append(List<String> list, String[] arr) {
+        if (arr.length > 2) {
+            System.out.println("Error: invalid command parameters");
+        } else {
+            list.add(arr[1]);
+            ListManager.printList(list);
+        }
+    }
+
+    static void prepend(List<String> list, String[] arr) {
+        if (arr.length > 2) {
+            System.out.println("Error: invalid command parameters");
+        } else {
+            list.add(0, arr[1]);
+            ListManager.printList(list);
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,16 +9,19 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        List<String> list = Arrays.asList(reader.readLine().split("\\s+"));
+        List<String> list = new ArrayList<>(Arrays.asList(reader.readLine().split("\\s+")));
         ListManager.printList(list);
 
         while (true) {
             String[] commandArgs = reader.readLine().split("\\s+");
-            if (commandArgs[0].equals("end")) {
-                break;
-            }
 
             switch (commandArgs[0]) {
+                case "append":
+                    ListManager.append(list, commandArgs);
+                    break;
+                case "prepend":
+                    ListManager.prepend(list, commandArgs);
+                    break;
 
             }
 
